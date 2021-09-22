2af4e67b1635d54ff7ad3c8ae093af6de9914a5e
jsan <ahsan.wtc@gmail.com>
2021-09-22 14:15:06 +0200

    added unstaking

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,6 +7,7 @@ contract TokenFarm {
   string public name = "DApp Token Farm";
   DaiToken public daiToken;
   DappToken public dappToken;
+  address public owner;
 
   address[] public stakers;
   mapping(address => uint) public stakingBalance;
@@ -16,9 +17,13 @@ contract TokenFarm {
   constructor (DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
+    owner = msg.sender;
   }
 
   function stakeTokens(uint _amount) public {
+    // Require amount greater than 0
+    require(_amount > 0, "amount can't be 0");
+
     // Transfer Mock Dai tokens to this contract fro staking
     daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -33,6 +38,35 @@ contract TokenFarm {
     // Updating staking status
     isStaking[msg.sender] = true;
     hasStaked[msg.sender] = true;
+  }
+
+  function issueTokens() public {
+    require(msg.sender == owner, "caller must be owner");
+    
+    for(uint i = 0; i < stakers.length; i++) {
+      address recipient = stakers[i];
+      uint balance = stakingBalance[recipient];
+      if (balance > 0) {
+        dappToken.transfer(recipient, balance);
+      }
+    }
+  }
+
+  function unstakeTokens() public {
+    // Fetch staking balance
+    uint balance = stakingBalance[msg.sender];
+
+    // Require amount greater than 0
+    require(balance > 0, "staking balance cannot be 0");
+
+    // Transfer Mock Dai tokens to caller
+    daiToken.transfer(msg.sender, balance);
+
+    // Reset staking balance
+    stakingBalance[msg.sender] = 0;
+
+    // Update staking status
+    isStaking[msg.sender] = false;
   }
 
 }
