cdeae81e8ce8500e3a27f51330fcaca0565a84b1
jsan <ahsan.wtc@gmail.com>
2021-09-22 12:00:42 +0200

    added migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,26 @@
 // eslint-disable-next-line no-undef
 const TokenFarm = artifacts.require('TokenFarm');
+// eslint-disable-next-line no-undef
+const DappToken = artifacts.require('DappToken');
+// eslint-disable-next-line no-undef
+const DaiToken = artifacts.require('DaiToken');
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Dai Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Transfer all tokens to TokenFarm (1 Million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+  // Transfer 100 Mock Dai tokens to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000');
 };
