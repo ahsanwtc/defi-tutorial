fab3811f58a8990850c84fb7ccfc9a795de4b58b
jsan <ahsan.wtc@gmail.com>
2021-09-22 13:46:08 +0200

    added staking function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,8 +8,31 @@ contract TokenFarm {
   DaiToken public daiToken;
   DappToken public dappToken;
 
+  address[] public stakers;
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaking;
+
   constructor (DappToken _dappToken, DaiToken _daiToken) public {
     dappToken = _dappToken;
     daiToken = _daiToken;
   }
+
+  function stakeTokens(uint _amount) public {
+    // Transfer Mock Dai tokens to this contract fro staking
+    daiToken.transferFrom(msg.sender, address(this), _amount);
+
+    // Updating staking balance
+    stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+    // Add user to stakers array iff they haven't staked already
+    if (!hasStaked[msg.sender]) {
+      stakers.push(msg.sender);
+    }
+
+    // Updating staking status
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+  }
+
 }
